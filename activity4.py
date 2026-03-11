30bd7201058e472f16edbcee775ced98559080f0
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-03-11 18:18:18 +0530

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number to check:"))
+print("Number to be checked :", number)
+
+if number%2==0 :
+    print("This is an Even number")
+
+else:
+    print("This is and Odd number")
