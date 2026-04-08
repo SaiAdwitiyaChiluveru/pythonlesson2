408914f0401e7c9a07951d85cc29373661b8f6cb
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-04-08 18:18:24 +0530

    Add files via upload

--- activity2.py
+++ activity2.py
@@ -1,20 +1,6 @@
-import turtle
-turtle.Screen().bgcolor("Aqua")
-board = turtle.Turtle()
-board.forward(100)
-board.left(120)
-board.forward(100)
-board.left(120)
-board.forward(100)
-board.penup()
-board.right(150)
-board.forward(50)
-board.pendown()
-board.right(90)
-board.forward(100)
-board.right(120)
-board.forward(100)
-board.right(120)
-board.forward(100)
-
-turtle.done()
+def weather_condition():
+    print("Weather condition is pleasant in:", spring)
+    print("The weather is same in:", autumn)
+spring = "autumn"
+autumn = spring
+weather_condition()
