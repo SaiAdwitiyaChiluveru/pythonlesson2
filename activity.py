f1d4a8ec2f30cb8cb8785f90dc93dafb4d50c60d
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-04-15 18:28:38 +0530

    Add files via upload

--- activity.py
+++
@@ -1,6 +0,0 @@
-def calculate_circumference(radius):
-    pi = 3.14159
-    return 2 * pi * radius
-r = float(input("Enter the radius: "))
-circumference = calculate_circumference(r)
-print("The circumference is: ", circumference)
