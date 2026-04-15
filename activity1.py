b2b8718017ff78251a75766ad0aa43c05192c7f7
Sai Adwitiya Chiluveru <kcsabitha_mct@mgit.ac.in>
2026-04-15 18:07:05 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,5 +1,7 @@
-def total_calc(bill_amount,tip_perc):
-    total = bill_amount*(1 + 0.01*tip_perc)
-    total = round(total,2)
-    print(f"Please pay ${total}")
-total_calc(150,20)
+a = input("Enter a word: ")
+for i in a:
+    if i == 'A':
+        print("A is found")
+        break 
+else:
+    print("A not found")
--- activity3.py
+++ activity3.py
@@ -1,19 +1,7 @@
-def factorial(x):
-    '''this is a recursive function to find the factorial of an integer'''
-    if x == 0:
-        return 1
-    else:
-        return x * factorial(x-1)
-print(factorial.__doc__)
-print("the factorial of 0:",factorial(0))
-print("the factorial of 1:",factorial(1))
-print("the factorial of 2:",factorial(2))
-print("the factorial of 5:",factorial(5))
-print("the factorial of 10:",factorial(10))
-print("the factorial of 954:",factorial(954))
-print("the factorial of 25:",factorial(25))
-print("the factorial of 23:",factorial(23))
-print("the factorial of 34:",factorial(34))
-print("the factorial of 27:",factorial(27))
-print("the factorial of 2925:",factorial(2925))
-print("the factorial of 12984:",factorial(12984))
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 5:
+        continue
+    print('\nCurrent variable value :', var)
+print('\nGoodBye!')
